fb3f5355ffe3807437a7a4daa5e9c979b61dd83b
Clara Heinz <clarakheinz@gmail.com>
2021-07-20 19:11:39 -0400

    styling fixes and condensing

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+// nav functions
+let hamburger = document.querySelector(".hamburger-icon");
+let navi = document.querySelector(".main-nav");
+
+hamburger.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    hamburger.classList.toggle("active");
+    navi.classList.toggle("active");
+}
+
+// get the nav links to close the menu on click//
+let navLink = document.querySelectorAll(".menu-item");
+
+for (let i=0; i < navLink.length; i++) {
+    navLink[i].addEventListener("click", closeMenu)
+};
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navi.classList.remove("active");
+}
+
+
 // need to create function to insert a random picture for profle
 //get html elemet
 let profileContainer = document.getElementById("name-pic");
